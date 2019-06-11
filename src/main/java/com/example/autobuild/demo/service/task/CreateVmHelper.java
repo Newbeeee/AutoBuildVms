4d3cfe8dfbe867b69add79bee58fba3c860b9741
newbeeee <1523955650@qq.com>
2019-06-11 15:23:50 +0800

    引入guava重试包，完善创建虚拟机失败重试

--- src/main/java/com/example/autobuild/demo/service/task/CreateVmHelper.java
+++ src/main/java/com/example/autobuild/demo/service/task/CreateVmHelper.java
@@ -1,46 +1,76 @@
 package com.example.autobuild.demo.service.task;
 
 import com.example.autobuild.demo.util.ServerManager;
+import com.github.rholder.retry.*;
+import com.google.common.base.Predicates;
 import org.openstack4j.model.compute.Server;
 
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class CreateVmHelper {
+
+
     public static Server createVm(String name, String flavorId, String imageId,
-                           List<String> networks,
-                           String securityGroup,
-                           String keypairName, ServerManager serverManager) {
+                                  List<String> networks,
+                                  String securityGroup,
+                                  String keypairName, ServerManager serverManager) {
 
         Server server = null;
+
+        // 第一次创建虚拟机
+        Callable<Server> firstCreateCall = new Callable<Server>() {
+            @Override
+            public Server call() {
+                return serverManager.createServer(name, flavorId, imageId, networks, securityGroup, keypairName);
+            }
+        };
+
+        // 重试策略
+        Retryer<Server> retryer = RetryerBuilder.<Server>newBuilder()
+                // 如果发生异常则重试
+                .retryIfException()
+                // 如果得到的server 为null则重试
+                .retryIfResult(Predicates.equalTo(null))
+                // 每次等待10秒后再重试
+                .withWaitStrategy(WaitStrategies.fixedWait(10, TimeUnit.SECONDS))
+                // 一共重试3次
+                .withStopStrategy(StopStrategies.stopAfterAttempt(3))
+                .build();
+
         try {
-            server = serverManager.createServer(name, flavorId, imageId, networks, securityGroup, keypairName);
-        } catch (Exception e) {
+            server = retryer.call(firstCreateCall);
+        } catch (ExecutionException e) {
             e.printStackTrace();
+        } catch (RetryException e) {
             System.out.println("创建虚拟机阶段出错: " + e.getMessage());
+            e.printStackTrace();
             return null;
         }
 
+        Server finalServer = server;
+        // 等待虚拟机状态为 active
+        Callable<Server> waitForActiveCall = new Callable<Server>() {
+            @Override
+            public Server call() {
+                return serverManager.waitForServerStatus(finalServer.getId(), Server.Status.ACTIVE, 2, TimeUnit.MINUTES);
+            }
+        };
 
         Server result = null;
 
         try {
-            /**
-             * 启动虚拟机并等待状态为active，最多等待2分钟
-             * 如果2分钟还没创建好，每隔30秒请求一次
-             * 直到状态为active
-             * 这里还需要捕获一些异常，如资源不够，配额不够等
-             * 但暂时还没遇到
-             */
-            result = serverManager.waitForServerStatus(server.getId(), Server.Status.ACTIVE, 2, TimeUnit.MINUTES);
-            while (result.getStatus() == null) {
-                result = serverManager.waitForServerStatus(server.getId(), Server.Status.ACTIVE, 30, TimeUnit.SECONDS);
-            }
-        } catch (Exception e) {
+            result = retryer.call(waitForActiveCall);
+        } catch (ExecutionException e) {
             e.printStackTrace();
+        } catch (RetryException e) {
             System.out.println("等待虚拟机运行状态出错: " + e.getMessage());
+            e.printStackTrace();
             return null;
         }
+
         return result;
     }
 }
